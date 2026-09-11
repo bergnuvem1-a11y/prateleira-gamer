e92332493c1d053c0b45ba2db0c60a721a540e52
bergnuvem1-a11y <bergnuvem1@gmail.com>
2026-09-11 16:19:41 -0300

    Update google-config.js

--- google-config.js
+++ google-config.js
@@ -1,10 +1,10 @@
 // Configuração do Google Drive API
 const GOOGLE_CONFIG = {
-    CLIENT_ID: '470753417515-o50b6gdekm4m2pj62lpkdjq54v02to8b.apps.googleusercontent.com',
+    CLIENT_ID: '470753417515-88dvms61pihr98ira5vkajdgqj1ino29.apps.googleusercontent.com',
     API_KEY: 'AIzaSyAkWSLubQhj1o8Ub_j4YId0daqm5Xqt-GA',
     DISCOVERY_DOCS: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
     SCOPES: 'https://www.googleapis.com/auth/drive.readonly',
-    
+
     // Nome da pasta no Google Drive onde estão os saves do CloudRedirect
     CLOUDREDIRECT_FOLDER: 'CloudRedirect'
 };
